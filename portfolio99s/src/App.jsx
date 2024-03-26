41761c60dc8fcaf18720088f06c3c6438d739e71
Adamz99s <adamyussof.marzuki@gmail.com>
2024-03-26 09:17:17 +0800

    change background

--- portfolio99s/src/App.jsx
+++ portfolio99s/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Profile from "./assets"
 import './App.css'
 
 function App() {
@@ -9,21 +8,42 @@ function App() {
   return (
     <>
       <header>
-        <div className='container m-auto'>
-          <div className='flex'>
+        <div className='container m-auto px-4 py-6'>
+          <div className='flex justify-between items-start'>
             <div>
-              <h1>Adam's Portfolio</h1>
+              <h1 className='font-bold text-xl'>Adam's Portfolio</h1>
             </div>
             <div>
-              <ul>
+              <ul className='flex gap-4'>
                 <li>
-                  <a className='text-gray-400 hover:text-white'> Projects</a>
+                  <a className='text-gray-400 hover:text-white cursor-pointer'> Projects</a>
+                </li>
+                <li>
+                <a className='text-gray-400 hover:text-white cursor-pointer'> Technologies</a>
+                </li>
+                <li>
+                <a className='text-gray-400 hover:text-white cursor-pointer'> About me</a>
                 </li>
               </ul>
             </div>
           </div>
         </div>
       </header>
+
+      {/*Main here */}
+      <main>
+
+        <section>
+          <div className='container m-auto px-4 py-10 flex'>
+          <div>
+
+          </div>
+          <div>
+            <img src={Profile} alt="Profile Image" />
+          </div>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
