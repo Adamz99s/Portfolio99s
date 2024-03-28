da8b2118978ed8cde259531ab2709015e26cc3f9
Adamz99s <adamyussof.marzuki@gmail.com>
2024-03-28 18:28:59 +0800

    Added Project Image into the website.

--- portfolio99s/src/App.jsx
+++ portfolio99s/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Profile from "./assets/Adamimg.jpg"
+import Biowebproj from "./assets/Biowebproj.jpg"
+
 import './App.css'
 
 function App() {
@@ -38,12 +40,31 @@ function App() {
           <div>
           <h1 className='font-bold text-4xl'>Hello, Im Adam,</h1>
           <h1 className='font-bold text-4x1 mt-1 gradient-text'>Junior Web Developer.</h1>
-          <p className='mt-4 text-gray-400'>A fresh graduates , curently seeking experiences</p>
+          <p className='mt-4 text-gray-400'>A fresh graduates , who loves to code.</p>
           </div>
           <div className='ml-5'>
             <img src={Profile} style={{width: 200, height:200, borderRadius: 200 / 2}} width="220" alt="Profile Image"  />
           </div>
           </div>
+        </section>
+        {/*PROJECT SECTION */}
+        <section>
+          <div className='container m-auto cursor-pointer'>
+            <h2>Projects</h2>
+            <div className='flex gap-10 mt-5'>
+            <div className='border rounded-md p-5'>
+
+              <img src={Biowebproj} style={{width:400}} alt="Bioweb project img" />
+              <h3>BioDyslexia.com</h3>
+              <p className='text-gray-400'>A Schooling System that focused on Children with Autism</p>
+
+            </div>
+            <div>
+                {/*IMPLEMENT NEXT IMG HERE */}
+            </div>
+            </div>
+
+         </div>
         </section>
       </main>
     </>
