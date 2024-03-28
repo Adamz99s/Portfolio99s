8600400cf3ae866a237924af041c31de34f3da3f
Adamz99s <adamyussof.marzuki@gmail.com>
2024-03-29 00:06:50 +0800

    Adjusting paragraphs and add button inside the rounded bar

--- portfolio99s/src/App.jsx
+++ portfolio99s/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
           <div className='container m-auto px-4 py-10 flex gap-6'>
           <div>
           <h1 className='font-bold text-4xl'>Hello, Im Adam,</h1>
-          <h1 className='font-bold text-4x1 mt-1 gradient-text'>Junior Web Developer.</h1>
+          <h1 className='font-bold text-4xl mt-1 gradient-text'>Junior Web Developer.</h1>
           <p className='mt-4 text-gray-400'>A fresh graduates , who loves to code.</p>
           </div>
           <div className='ml-5'>
@@ -50,14 +50,16 @@ function App() {
         {/*PROJECT SECTION */}
         <section>
           <div className='container m-auto cursor-pointer'>
-            <h2>Projects</h2>
+            <h2 className='text-2xl font-semibold'>Projects</h2>
             <div className='flex gap-10 mt-5'>
             <div className='border rounded-md p-5'>
 
               <img src={Biowebproj} style={{width:400}} alt="Bioweb project img" />
-              <h3>BioDyslexia.com</h3>
-              <p className='text-gray-400'>A Schooling System that focused on Children with Autism</p>
-
+              <h3 className='text-2xl font-semibold mt-8'>BioDyslexia.com</h3>
+              <p className='text-gray-400 text-sm mt-2'>A Schooling System that focused on Children with Autism</p>
+              <div className='flex'>
+              <button className='p-5 border rounded-full mt-5'>Preview Here</button>
+              </div>
             </div>
             <div>
                 {/*IMPLEMENT NEXT IMG HERE */}
